0daedef477077002d8443313e11c302ae9fba14c
Ocyss_04 <68412205+Ocyss@users.noreply.github.com>
2025-12-05 01:18:28 +0800

    [ClashMeta]去除rule添加

--- app/Protocols/ClashMeta.php
+++ app/Protocols/ClashMeta.php
@@ -132,7 +132,7 @@ class ClashMeta extends AbstractProtocol
         }
 
         $config['proxy-groups'] = array_values($config['proxy-groups']);
-        $config = $this->buildRules($config);
+        # $config = $this->buildRules($config);
 
         $yaml = Yaml::dump($config, 2, 4, Yaml::DUMP_EMPTY_ARRAY_AS_SEQUENCE);
         $yaml = str_replace('$app_name', admin_setting('app_name', 'XBoard'), $yaml);
